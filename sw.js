f03d3c2f630b9a55dc5d1616eb6c70d1753eaf01
sadisalah24-design <sadisalah24@gmail.com>
2026-08-25 02:02:31 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const CACHE="backwards-clock-v2";const ASSETS=["./","./index.html","./manifest.webmanifest"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)))})
+const C="bc-v3";const A=["./","./index.html","./manifest.webmanifest","./sw.js"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)))});
