fefbf1a66591bf99a6dd5ccc945bef7d9a3cda57
sadisalah24-design <sadisalah24@gmail.com>
2026-08-26 21:28:53 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const C="bc-v3";const A=["./","./index.html","./manifest.webmanifest","./sw.js"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)))});
+const C="backward-clock-v3";const A=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
