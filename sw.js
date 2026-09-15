9309697983c44c6e663925c829021b7ddb44c3d7
Desprite <zeroxfourtyfour@proton.me>
2026-09-15 18:52:44 +0300

    Upgrade service worker to version 7
    Updated service worker to use new cache version and assets.

--- sw.js
+++ sw.js
@@ -1,5 +1,27 @@
-const CACHE="backward-clock-v4";
-const ASSETS=["./","./index.html","./styles.css?v=4","./app.js?v=4","./manifest.webmanifest?v=4","./icon.svg?v=4"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE="backward-clock-v7";
+const ASSETS=["./manifest.webmanifest?v=7","./icon.svg?v=7"];
+
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))
+});
+
+self.addEventListener("activate",e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  )
+});
+
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,copy));
+        return res;
+      })
+      .catch(()=>caches.match(e.request))
+  );
+});
